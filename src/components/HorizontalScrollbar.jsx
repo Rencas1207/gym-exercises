01d23e0df1b543d74e360d6b0185870eef175f4a
Rencas <renzo.c@ohecommerce.agency>
2023-05-21 16:10:33 -0500

    gym-exercises body part

--- src/components/HorizontalScrollbar.jsx
+++ src/components/HorizontalScrollbar.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import { useContext } from 'react'
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 import { Box, Typography } from '@mui/material';
+
 import { BodyPart } from './BodyPart';
+import RightArrowIcon from '../assets/icons/right-arrow.png';
+import LeftArrowIcon from '../assets/icons/left-arrow.png'
+
+const LeftArrow = () => {
+   const { scrollPrev } = useContext(VisibilityContext);
+
+   return (
+      <Typography onClick={() => scrollPrev()} className="right-arrow">
+         <img src={LeftArrowIcon} alt="right-arrow" />
+      </Typography>
+   );
+};
+
+const RightArrow = () => {
+   const { scrollNext } = useContext(VisibilityContext);
+
+   return (
+      <Typography onClick={() => scrollNext()} className="left-arrow">
+         <img src={RightArrowIcon} alt="right-arrow" />
+      </Typography>
+   );
+};
 
 export const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
    return (
-      <div>
+      <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
          {
             data.map((item) => (
                <Box
@@ -20,6 +44,6 @@ export const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
                   />
                </Box>
             ))}
-      </div>
+      </ScrollMenu>
    )
 }
