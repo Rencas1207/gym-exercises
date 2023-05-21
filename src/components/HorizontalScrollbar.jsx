ed5803306772a3db4be0f1aa90f61e73e9f7117c
Rencas <renzo.c@ohecommerce.agency>
2023-05-21 01:56:22 -0500

    gym-exercises horizontal scrollbar

--- src/components/HorizontalScrollbar.jsx
+++ src/components/HorizontalScrollbar.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { Box, Typography } from '@mui/material';
+import { BodyPart } from './BodyPart';
 
-export const HorizontalScrollbar = () => {
+export const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
    return (
-      <div>HorizontalScrollbar</div>
+      <div>
+         {
+            data.map((item) => (
+               <Box
+                  key={item.id || item}
+                  itemId={item.id || item}
+                  title={item.id || item}
+                  m="0 40px"
+               >
+                  <BodyPart
+                     item={item}
+                     bodyPart={bodyPart}
+                     setBodyPart={setBodyPart}
+                  />
+               </Box>
+            ))}
+      </div>
    )
 }
