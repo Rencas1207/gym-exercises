ac6e7b32adaa700fe471283a713669063965b78f
Rencas <renzo.c@ohecommerce.agency>
2023-05-21 19:28:17 -0500

    gym-exercises detail component

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -2,13 +2,14 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 
-import { exerciseOptions, fetchData } from '../utils/fetchData';
+import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
 import { Detail } from '../components/Detail';
 import { ExerciseVideos } from '../components/ExerciseVideos';
 import { SimilarExercises } from '../components/SimilarExercises';
 
 export const ExerciseDetail = () => {
    const [exerciseDetail, setExerciseDetail] = useState({});
+   const [exerciseVideos, setExerciseVideos] = useState([]);
 
    const { id } = useParams();
 
@@ -20,6 +21,10 @@ export const ExerciseDetail = () => {
          const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercise/${id}`, exerciseOptions);
 
          setExerciseDetail(exerciseDetailData);
+
+         const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`, youtubeOptions);
+
+         setExerciseVideos(exerciseVideosData);
       }
 
       fetchExercisesData();
@@ -27,8 +32,8 @@ export const ExerciseDetail = () => {
 
    return (
       <Box >
-         <Detail />
-         <ExerciseVideos />
+         <Detail exerciseDetail={exerciseDetail} />
+         <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
          <SimilarExercises />
       </Box>
    )
